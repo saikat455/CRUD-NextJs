160107ad8fecbcbbc1893eec2aa6874f0b8af64f
Saikat <saikat25112000@gmail.com>
2024-09-10 01:43:41 +0600

    updated env

--- libs/mongodb.js
+++ libs/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectMongoDB = ()=>{
     try{
-        mongoose.connect(process.env.MONGODB_URI);
+        mongoose.connect(process.env.NEXT_PUBLIC_MONGODB_URI);
         console.log("Connected to MongoDB");
     }
     catch (error){
